9e2f2db818a671cfdef41f7b76d8090aad7e11d6
gabogg21 <gabrielgiron@ucb.edu.bo>
2025-04-23 22:01:50 -0400

    Funciones biblioteca

--- Tareas/practicoEjAvanzado/Funciones.c
+++ Tareas/practicoEjAvanzado/Funciones.c
@@ -1,6 +1,3 @@
-// {} #
-// \n*cac
-
 //aqui no puede haber un int main, ni bucles infinitos
 #include <stdio.h> 
 #include "funciones.h"
@@ -32,18 +29,63 @@ int division(int numero7, int numero8){
     return resp3;
 }
 
-int par(int num1){
-    if (num1 %2 == 0){
-
-    }return printf("El numero es par\n");
-
+int parimpar(int num1){
+    return num1%2==0;
 }
 
-int impar(int num2){
-    if (num2 %2 != 0){
+int digitos(int numing, int cont){
+    while(numing!=0){
+        numing= numing /10;
+        cont++;
+    }
 
+    return cont;
+}
+
+int sumaint(int numint, int cont2){
+
+    if(numint >=0){
+        cont2 = cont2 + numint;
+    }
+    return cont2;
+}
+
+int mayor(int may, int men){
     
-
-    }return printf("El numero es impar\n");
+        return may>men;   
 }
 
+int menor(int may2, int men2){
+    
+    return may2<men2;
+}
+
+int igual(int may3, int men3){
+    
+    return may3==men3;
+}
+
+int multiplosDe3(int numb, int cont3){
+    if(numb %3==0){
+        cont3++;
+    }
+    return cont3;
+}
+
+int factorial(int n, int limite,int total){
+    for(;n<=limite;n++){
+        total= total*n;
+
+
+    }return total;
+}
+
+int invertirNumero(int n){
+    int invertido = 0;
+
+    for (; n > 0; n= n/10) {
+        invertido = invertido * 10 + n % 10;
+    }
+
+    return invertido;
+}
--- Tareas/practicoEjAvanzado/funciones.h
+++ Tareas/practicoEjAvanzado/funciones.h
@@ -11,10 +11,22 @@ int multiplicacion(int numero5, int numero6);
 
 int division(int numero7, int numero8);
 
-int par(int num1);
+int parimpar(int num1);
 
-int impar(int num2);
+int digitos(int numing, int cont);
 
+int sumaint(int numint, int cont2);
 
+int mayor(int may, int men);
+
+int menor(int may2, int men2);
+
+int igual(int may3, int men3);
+
+int multiplosDe3(int numb, int cont3);
+
+int factorial(int n, int limite,int total);
+
+int invertirNumero(int n);
 #endif // para que el archivo solo sea llamado una vez, sirve para q no se repita dentro
 //de un bucle por ejmemplo
